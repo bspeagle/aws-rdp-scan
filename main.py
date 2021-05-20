c164e9b18b86fbbfa2cdfaab9ccfd8367c217f9b
bspeagle <bspeagle@gmail.com>
2021-05-20 16:02:28 -0400

    mostly works

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from logging import exception
 import boto3
 import os
 import re
@@ -7,6 +8,7 @@ import xml.dom.minidom
 open_re = re.compile(r"Security\slayer")
 closed_re = re.compile(r"^Received\sunhandled\spacket")
 status_dict = {}
+output_list = []
 
 def readXMLFile(inputfilename):
     DOMTree = xml.dom.minidom.parse(inputfilename)
@@ -19,15 +21,18 @@ def parseXMLFile(DOMTree):
         ip_addr = portscan.getElementsByTagName('host')[0].getElementsByTagName('address')[0].getAttribute('addr')
         for port in ports:
             if (port.getAttribute("portid") == "3389"):
-                script_output = port.getElementsByTagName('script')[0].getAttribute('output')
+                try:
+                    script_output = port.getElementsByTagName('script')[0].getAttribute('output')
+                except:
+                    script_output = ''
         if open_re.search(script_output):
             rdp_status = "Accessible"
         elif closed_re.match(script_output):
             rdp_status = "Inaccessible"
         else:
             rdp_status = "Undetermined"
-    except:
-        print("[-] Error parsing the output.xml file.")
+    except Exception as ex:
+        print("[-] Error parsing the output.xml fileL: " + str(ex))
         ip_addr = ""
         rdp_status = "fail"
     status_dict[ip_addr] = rdp_status
@@ -37,7 +42,9 @@ def parse_output():
     ip_addr, rdp_status = parseXMLFile(readXMLFile("output.xml"))
     if not(rdp_status == "fail"):
         print("[+]", str(ip_addr), "-", str(rdp_status))
-    os.remove("output.xml")
+    
+    output_list.append(str(ip_addr) + " - " + str(rdp_status))
+    # os.remove("output.xml")
 
 def check_rdp(ip_addr):
     try:
@@ -45,8 +52,8 @@ def check_rdp(ip_addr):
         nmap_process = subprocess.Popen(nmap_command, stdout=subprocess.PIPE, shell=True)
         nmap_process.communicate()
         parse_output()
-    except Exception(ex):
-        print("[-] Error occured while running script: " + ex)
+    except Exception as ex:
+        print("[-] Error occured while running script: " + str(ex))
 
 ec2client_1 = boto3.client('ec2')
 region_response = ec2client_1.describe_regions()
@@ -59,3 +66,5 @@ for region in region_response['Regions']:
         for instance in reservation["Instances"]:
             if instance['State']['Name'] == 'running':
                 check_rdp(instance['PublicIpAddress'])
+
+print(output_list)
